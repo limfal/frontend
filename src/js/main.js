075ced003eb257d086b1f1e6a7b812b4a179056a
Linn Fällman <lifa2209@student.miun.se>
2025-04-14 14:08:11 +0200

    Add animation4 with js

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,15 @@
 "use strict";
 
 console.log("Hello world!");
+
+let btnEl = document.getElementById("clickme");
+let boxEl = document.getElementById("animation4");
+
+btnEl.addEventListener('click', runPlay) 
+
+function runPlay () {
+
+let style = window.getComputedStyle(boxEl);
+
+boxEl.style.animation = "bubblecolor 1s 1s infinite alternate running";
+}

be1d6b45e21b4f421cb7ae2fb023f5d6e592db0d
Linn Fällman <lifa2209@student.miun.se>
2025-04-15 12:38:06 +0200

    Add pause function to button

--- src/js/main.js
+++ src/js/main.js
@@ -7,9 +7,22 @@ let boxEl = document.getElementById("animation4");
 
 btnEl.addEventListener('click', runPlay) 
 
-function runPlay () {
+// function runPlay () {
 
-let style = window.getComputedStyle(boxEl);
+//     let style = window.getComputedStyle(boxEl);
+    
+//     boxEl.style.animation = "bubblecolor 1s 1s infinite alternate running";
+//     }
 
-boxEl.style.animation = "bubblecolor 1s 1s infinite alternate running";
-}
+    function runPlay () {
+
+        let style = window.getComputedStyle(boxEl);
+        
+        if (boxEl.style.animationPlayState === "paused") {
+            boxEl.style.animationPlayState = "running";
+        } else {boxEl.style.animationPlayState = "paused";
+        }
+        }
+
+//If animationthingie === se above then change to paused
+//else change to it

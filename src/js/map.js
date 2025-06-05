ada2e2cb371afa4857b9ed9c1e4ed1a25f09c387
Linn Fällman <lifa2209@student.miun.se>
2025-06-05 14:17:59 +0200

    Start adding js for nominatim search

--- src/js/map.js
+++ src/js/map.js
@@ -1,32 +1,52 @@
 "use strict";
 
+// GEOLOCATION STUFF
 if ("geolocation" in navigator) {
 
     navigator.geolocation.getCurrentPosition(function (position) {
-    let latitude = position.coords.latitude;
-    let longitude = position.coords.longitude;
-    
-    loadMap(latitude, longitude);
+        let latitude = position.coords.latitude;
+        let longitude = position.coords.longitude;
+
+        loadMap(latitude, longitude);
     }, function (error) {
-    console.error("Fel vid hämtning av postition:", error.message);
-    let latitude = "62.39";
-    let longitude = "17.29";
-    loadMap(latitude, longitude);
+        console.error("Fel vid hämtning av postition:", error.message);
+        let latitude = "62.39";
+        let longitude = "17.29";
+        loadMap(latitude, longitude);
     });
-    } else {
+} else {
     console.error("Geolokalisering stöds inte av din webbläsare");
     let latitude = "50.5380";
     let longitude = "12.0471";
     loadMap(latitude, longitude);
-    }
-    
-    
-    function loadMap (long, lat) {
+}
+
+// VARIABLER
+const mapEl = document.getElementById("map");
+const btnEl = document.getElementById("searchBtn");
+const inputEl = document.getElementById("mapSearch");
+
+btnEl.addEventListener('click', searchPlace);
+
+
+function searchPlace () {
+    console.log(inputEl.value);
+
+    //HÄR LÄGGER VI IN HELA NOMINATIMSÖKNINGEN
+}
+
+
+// FUNKTION FÖR ATT VISA KARTAN
+function loadMap(long, lat) {
+
+    mapEl.innerHTML = "";
+
     const map = L.map('map').setView([long, lat], 13);
-    
+
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+        maxZoom: 19,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     }).addTo(map);
-    
-    }
+
+}
+
